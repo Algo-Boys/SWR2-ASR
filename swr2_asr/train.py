2f6037861f32ed2721fe66901dafc67aa3cad5b9
Marvin Borner <git@marvinborner.de>
2023-08-18 14:07:10 +0200

    Fixed @Jojobarthold2 hallo!!!

--- swr2_asr/train.py
+++ swr2_asr/train.py
@@ -1,11 +1,12 @@
 """Training script for the ASR model."""
 from AudioLoader.speech.mls import MultilingualLibriSpeech
+import os
 
 
 def main() -> None:
     """Main function."""
     dataset = MultilingualLibriSpeech(
-        "data", "mls_polish_opus", split="train", download=True
+        "data", "mls_polish_opus", split="train", download=(not os.path.isdir("data"))
     )
 
     print(dataset[1])
